2ece4fd2e8345cafbcb4524a9d3f6f7915e83c2e
Ravikiran <raviramesh.kiran97@gmail.com>
2017-07-07 10:24:25 +0530

    seperated and used anim for animation

--- complete_pedestrian_simulation.py
+++ complete_pedestrian_simulation.py
@@ -1,8 +1,9 @@
 """
 The following commands are for importing the required packages to run the script.
-You will need to install the following modules in python : numpy, openpyxl,matplotlib,pandas
+You will need to install the following modules in python : numpy, openpyxl, xlrd, matplotlib, pandas
 """
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import math
 import pandas as pd
 import numpy as np
@@ -15,7 +16,7 @@ t_int=0.05     #interval time (in seconds)
 t_total=20     #total running time (in seconds)
 loops=int(t_total/t_int) #calculates the number of loops to be performed
 grid_size=1  #size of grid in the heat map (in meters)
-n_ped=200   #number of pedestrians
+n_ped=50   #number of pedestrians
 m_ped=1    #mass of one pedestrian
 r_p=0.3   #radius of one pedestrain (in meters)
 v_desired_max=1.4 #desired speed of the pedestrian (in m/s)
@@ -60,6 +61,8 @@ class Pedestrian:
 		self.set_path()
 		self.vx=0    #x velocity
 		self.vy=0    #y velocity
+		self.ax=0    #x acceleration
+		self.ay=0    #y acceleration
 
 		self.init_constants() #invokes the function init_constants 
 		self.side=self.calc_side() #invokes the function calc side and assign it to the variable side
@@ -139,7 +142,7 @@ class Pedestrian:
 		self.point=0
 		self.end=False
 		self.start=False
-		self.starting_frame=random.randint(0,loops)
+		self.starting_frame=random.randint(0,loops/2)
 
 		self.t_relax=random.uniform(0.9,1.1)
 		self.rad=random.uniform(0.5,0.6)
@@ -176,9 +179,6 @@ def driving_force(p):
 	"""Constants"""
 	#t_relax=1  #relaxation time
 
-	if p.start==False or p.end==True:
-		return 0.0,0.0
-
 	df_x=(p.vdx-p.vx)/p.t_relax  #force in the x direction
 	df_y=(p.vdy-p.vy)/p.t_relax  #force in the y direction
 	return df_x,df_y
@@ -196,8 +196,6 @@ def border_repulsion(p):
 	f_x=0
 	f_y=0
 
-	if p.start==False or p.end==True:
-		return f_x,f_y
 
 	for row in walls.itertuples():
 		A=row[2]-row[4]                #we are finding the perpendicular distance between the wall and the pedestrian
@@ -241,8 +239,6 @@ def pedestrian_repulsion(ped,p):
 	f_x=0 # net force in the x direction
 	f_y=0 # net force in the y direction
 
-	if p.start==False or p.end==True:
-		return f_x,f_y
 
 	for i in xrange(n_ped): #iterates through all the pedestrians from the list 
 		dist=math.sqrt((ped[i].x-p.x)**2+(ped[i].y-p.y)**2) #distance of the i th pedestrian from pedestrian p 
@@ -250,7 +246,7 @@ def pedestrian_repulsion(ped,p):
 		if dist==0: #distance is zero indicates that i th pedestrian is p 
 			continue #since the pedestrian does not exert any force on themselves
 		else:
-			if ped[i].start==True:
+			if ped[i].start==True and ped[i].end==False:
 				nx=(p.x-ped[i].x)/dist  #x component of unit vector between the two pedestrians
 				ny=(p.y-ped[i].y)/dist  #y component of unit vector between the two pedestrians
 				cos=abs(p.x*nx+p.y*ny)  # cos of angle between the line joining two pedestrian, and the path of p
@@ -269,71 +265,63 @@ def sfm(ped,frame):
 	It takes the list containing all the pedestrian instances as arguement, 
 	and updates the pedestrian kinematic values with the ones after a time of 't_int'   
 	"""
-	fx_total=[]   #stores the forces experienced by all the pedestrians in the x direction
-	fy_total=[]   #stores the forces experienced by all the pedestrians in the y direction
+	
 	for i in xrange(n_ped):  #this loop iterates through all the pedestrians and calculates the force on the pedestrians
 		#this statement calls the three force functions, and obtains the net force of each pedestrian in the x and y directions
+		#It then updates the positions and velocities of the pedestrians using kinematic equations
 		
-		ped[i].calc_desired_velocity()
-		f_total=[sum(x) for x in zip( driving_force(ped[i]) , border_repulsion(ped[i]) , pedestrian_repulsion(ped,ped[i]))]
-		fx_total.append(f_total[0])    #net force of all the pedestrians in the x direction
-		fy_total.append(f_total[1])    #net force of all the pedestrians in the y direction
+		if ped[i].start==True and ped[i].end==False:
+			ped[i].calc_desired_velocity()
+			f_total=[sum(x) for x in zip( driving_force(ped[i]) , border_repulsion(ped[i]) , pedestrian_repulsion(ped,ped[i]))]
+			ped[i].ax=f_total[0]/m_ped
+			ped[i].ay=f_total[1]/m_ped
 
-
-	for i in xrange(n_ped):    #this loop updates the position and velocity of each pedestrian using the forces obtained
-		
-		if ped[i].start==True and ped[i].end==False: 
-			ped[i].x+=ped[i].vx*t_int+0.5*(fx_total[i]/m_ped)*t_int*t_int  # s = ut + 0.5 at^2 in the x direction
-			ped[i].y+=ped[i].vy*t_int+0.5*(fy_total[i]/m_ped)*t_int*t_int  # s = ut + 0.5 at^2 in the y direction
-			ped[i].vx+=(fx_total[i]/m_ped)*t_int  # v = u + at in the x direction
-			ped[i].vy+=(fy_total[i]/m_ped)*t_int  # v = u + at in the y direction
+			ped[i].x+=ped[i].vx*t_int+0.5*ped[i].ax*t_int*t_int  # s = ut + 0.5 at^2 in the x direction
+			ped[i].y+=ped[i].vy*t_int+0.5*ped[i].ay*t_int*t_int  # s = ut + 0.5 at^2 in the y direction
+			ped[i].vx+=ped[i].ax*t_int  # v = u + at in the x direction
+			ped[i].vy+=ped[i].ay*t_int  # v = u + at in the y direction
 		else:
 			if frame==ped[i].starting_frame:
 				ped[i].start=True
 
+		
+		
+
 '''
 Functions for plotting the data
 '''
-def heat_maps(ped):
-	"""
-	This method takes the pedestrian details ped and generates heat map going from white to dark red
-	"""
-
+def update_heat_map(i,ax1,im):
 	cri=np.zeros(shape=(int(y_dim*mf),int(x_dim*mf))) #creates a numpy array for each cell of the heat map, and element value 0
 	density=np.zeros(shape=(int(y_dim*mf),int(x_dim*mf)))
 	impatience=np.zeros(shape=(int(y_dim*mf),int(x_dim*mf)))
 
+	ped_x=0
+	ped_y=0
+	ped_vx=0
+	ped_vy=0
+	imp=1
 
-	for j in xrange(n_ped): #iterates through each pedestrian
-		if ped[j].x>=0 and ped[j].x<=x_dim and ped[j].y>=0 and ped[j].y<=y_dim:
-			if ped[j].start==True and ped[j].end==False:
-				density[int(math.floor(ped[j].y*mf))][int(math.floor(ped[j].x*mf))]-=1 #decrements if pedestrian is present in the cell
-				impatience[int(math.floor(ped[j].y*mf))][int(math.floor(ped[j].x*mf))]=-ped[j].imp
+	for j in xrange(n_ped):
+		ped_x=positionX.loc[i][j]
+		ped_y=positionY.loc[i][j]
+		ped_vx=velocityX.loc[i][j]
+		ped_vy=velocityY.loc[i][j]
+		imp=1-(math.sqrt(ped_vx**2 + ped_vy**2))/v_desired_max
+		density[int(math.floor(ped_y*mf))][int(math.floor(ped_x*mf))]-=1 #decrements if pedestrian is present in the cell
+		impatience[int(math.floor(ped_y*mf))][int(math.floor(ped_x*mf))]-=imp
 
 	cri=density_weight*density + imp_weight*impatience
-	im1.set_data(cri) #sets data for the heat map
-	im1.axes.figure.canvas.draw() #draws the canvas
+	im.set_data(cri) #sets data for the heat map
+	return im,
 
-def animate(ped):
-	"""
-	This method takes the pedestian details ped and animates their motion
-	"""
-	x=[]
-	y=[]
-	ax2.cla()
 
-	for j in range(n_ped):
-		if ped[j].start==True and ped[j].end==False:
-			if ped[j].x<x_dim:
-				x.append(ped[j].x)
-				y.append(ped[j].y)
-
-	ax2.set_xlim(0,x_dim)
-	ax2.set_ylim(0,y_dim)
-	ax2.set_title("Pedestrian locus animation")
-	ax2.scatter(x,y,s=50,lw=0,facecolor='0.5')
-	for row in walls.itertuples():
-		ax2.plot([row[1],row[3]],[row[2],row[4]])
+def update_scatter_plot(i,ax2,scat):
+	ped_xy=[]
+	for j in xrange(n_ped):
+		if velocityX.loc[i][j]!=0 and velocityY.loc[i][j]!=0:
+			ped_xy.append([positionX.loc[i][j] , positionY.loc[i][j]])
+	scat.set_offsets(ped_xy)
+	return scat,
 
 '''
 Functions to generate and delete pedestrians
@@ -350,79 +338,118 @@ def generate_pedestrians(ped):
 
 def create_dataframe():
 	
-	positionX_col=[]  #column names for the pandas dataframe containing pedestrian y positions
-	positionY_col=[]  #column names for the pandas dataframe containing pedestrian x positions
-	impatience_col=[] #column names for the pandas dataframe containing pedestrian impatience indexes
+	positionX_col=[]  #column names for the pandas dataframe containing pedestrian x positions
+	positionY_col=[]  #column names for the pandas dataframe containing pedestrian y positions
+	velocityX_col=[]  #column names for the pandas dataframe containing pedestrian x velocities  
+	velocityY_col=[]  #column names for the pandas dataframe containing pedestrian y velocities
+	accelerationX_col=[]  #column names for the pandas dataframe containing pedestrian x accelerations
+	accelerationY_col=[]  #column names for the pandas dataframe containing pedestrian y accelerations
 
 	for i in xrange(n_ped): #iterates for n_ped number of times 
-		positionX_col.append("Pedestrian "+str(i+1)+" (X)") #adds the column label for x position
-		positionY_col.append("Pedestrian "+str(i+1)+" (Y)") #adds the column label for y position
-		impatience_col.append("Pedestrian "+str(i+1)+" (imp)") #adds the column label for impatience
+		positionX_col.append("Pedestrian "+str(i+1)) #adds the column label for x position
+		positionY_col.append("Pedestrian "+str(i+1)) #adds the column label for y position
+		velocityX_col.append("Pedestrian "+str(i+1)) #adds the column label for y position
+		velocityY_col.append("Pedestrian "+str(i+1)) #adds the column label for y position
+		accelerationX_col.append("Pedestrian "+str(i+1)) #adds the column label for y position
+		accelerationY_col.append("Pedestrian "+str(i+1)) #adds the column label for y position
+		
 
 	positionX=pd.DataFrame(columns=positionX_col) #contructs empty dataframe with the required columns 
 	positionY=pd.DataFrame(columns=positionY_col) #contructs empty dataframe with the required columns
-	impatience=pd.DataFrame(columns=impatience_col) #contructs empty dataframe with the required columns
+	velocityX=pd.DataFrame(columns=velocityX_col) #contructs empty dataframe with the required columns
+	velocityY=pd.DataFrame(columns=velocityY_col) #contructs empty dataframe with the required columns
+	accelerationX=pd.DataFrame(columns=accelerationX_col) #contructs empty dataframe with the required columns
+	accelerationY=pd.DataFrame(columns=accelerationY_col) #contructs empty dataframe with the required columns
 
-	return positionX,positionY,impatience
+	return positionX,positionY,velocityX,velocityY,accelerationX,accelerationY
 
-def add_dataframe(positionX,positionY,impatience):
+def add_dataframe(positionX,positionY,velocityX,velocityY,accelerationX,accelerationY):
 
 	positionX.loc[i]=[a.x for a in ped] #adds the x position of all the pedestrians to the data frame
 	positionY.loc[i]=[a.y for a in ped] #adds the y position of all the pedestrians to the data frame
-	impatience.loc[i]=[a.imp for a in ped] #adds the impatience of all the pedestrians to the data frame
+	velocityX.loc[i]=[a.vx for a in ped] #adds the x velocity of all the pedestrians to the data frame
+	velocityY.loc[i]=[a.vy for a in ped] #adds the y velocity of all the pedestrians to the data frame
+	accelerationX.loc[i]=[a.ax for a in ped] #adds the x acceleration of all the pedestrians to the data frame
+	accelerationY.loc[i]=[a.ay for a in ped] #adds the y acceleration of all the pedestrians to the data frame
+	
 
 
-def save_as_excel(positionX,positionY,impatience):
+def save_as_excel(positionX,positionY,velocityX,velocityY,accelerationX,accelerationY):
 	"""
 	This method saves the pandas dataframe into excel.
 	"""
 	writer=pd.ExcelWriter("Pedestrian_details.xlsx") #creates an excel writes
+
 	positionX.to_excel(writer,sheet_name="X positions") #writes the dataframe into the excel file in the given sheet
 	positionY.to_excel(writer,sheet_name="Y positions") #writes the dataframe into the excel file in the given sheet
-	impatience.to_excel(writer,sheet_name="impatience") #writes the dataframe into the excel file in the given sheet
+	velocityX.to_excel(writer,sheet_name="X velocity") #writes the dataframe into the excel file in the given sheet
+	velocityY.to_excel(writer,sheet_name="Y velocity") #writes the dataframe into the excel file in the given sheet
+	accelerationX.to_excel(writer,sheet_name="X acceleration") #writes the dataframe into the excel file in the given sheet
+	accelerationY.to_excel(writer,sheet_name="Y acceleration") #writes the dataframe into the excel file in the given sheet
+
 	writer.save() #saves the excel file in the same diretory as this script
 
+def read_from_excel():
+	positionX=pd.ExcelFile("Pedestrian_details.xlsx").parse("X positions")
+	positionY=pd.ExcelFile("Pedestrian_details.xlsx").parse("Y positions")
+	velocityX=pd.ExcelFile("Pedestrian_details.xlsx").parse("X velocity")
+	velocityY=pd.ExcelFile("Pedestrian_details.xlsx").parse("Y velocity")
+	accelerationX=pd.ExcelFile("Pedestrian_details.xlsx").parse("X acceleration")
+	accelerationY=pd.ExcelFile("Pedestrian_details.xlsx").parse("Y acceleration")
+	return positionX,positionY,velocityX,velocityY,accelerationX,accelerationY
 
 
+	
 
 
 if __name__=="__main__":
+
+	############## CALCULATION ########################
+	'''
 	ped=[]  #list of pedestrian instances
 
 	generate_pedestrians(ped)  #generate some pedestrians initially
-	positionX,positionY,impatience=create_dataframe() #create dataframes in which the pedestrian data at each instance is stored
+	positionX,positionY,velocityX,velocityY,accelerationX,accelerationY=create_dataframe() #create dataframes in which the pedestrian data at each instance is stored
 
+	for i in xrange (loops): #iterates through the loops
+		sfm(ped,i) #updates the pedestrian positions and velocity by calculating the forces 
+		add_dataframe(positionX,positionY,velocityX,velocityY,accelerationX,accelerationY)
+	
+	save_as_excel(positionX,positionY,velocityX,velocityY,accelerationX,accelerationY)
+
+	'''
+
+	############# ANIMATION ############################
+	positionX,positionY,velocityX,velocityY,accelerationX,accelerationY=read_from_excel()
 
 	fig=plt.figure(figsize=(10,5)) #sets the size of the figure of the plot
 	plt.suptitle("Animation graphs") #set title for graph
 
 	ax1=fig.add_subplot(1,2,1) #we create a 1 x 2 subplot and assigning ax1 to the first subplot
 	ax1.set_title("Heat map animation") #sets title to the subplot
-	ax1.invert_yaxis()  #inverts the y axis
 	ax1.set_xlim(0,x_dim*mf) #set the x scale of the heat map (first subplot)
 	ax1.set_ylim(0,y_dim*mf) #set the y scale of the heat map (first subplot)
 	ax1.get_xaxis().set_visible(False) #removes the scale display of the heat map
 	ax1.get_yaxis().set_visible(False) #removes the scale display of the heat map
- 
 	for row in walls.itertuples(): #for loop to iterate through the dataframe containing the wall coordinates
 		ax1.plot([row[1]*mf,row[3]*mf],[row[2]*mf,row[4]*mf])  #this will plot a line using the x and y coordinates of the wall entered in the excel
-
-	ax2=fig.add_subplot(1,2,2) #we create a 1 x 2 subplot and assigning ax2 to the second subplot
-	ax2.set_title("Pedestrian locus animation") #sets title to the subplot
-	for row in walls.itertuples(): #for loop to iterate through the dataframe containing the wall coordinates
-		ax2.plot([row[1],row[3]],[row[2],row[4]]) #this will plot a line using the x and y coordinates of the wall entered in the excel
-	
 
 	cri=np.zeros(shape=(int(y_dim*mf),int(x_dim*mf))) #creates a numpy array for each cell of the heat map, and element value 0
 	#This statement now creates a heat map corresponding to the pedestrian_map
 	#Here, hot varies from black to red to white. So, we want black if the pedestrian cri is max and white if it is minimum
-	im1=ax1.imshow(cri,cmap='hot', interpolation='nearest',vmin=-(max_cri),vmax=0) #we are using negativee value for cri since maximum is white and minimum is black when cmap is set to hot
+	im=ax1.imshow(cri,cmap='hot', interpolation='nearest',vmin=-(max_cri),vmax=0) #we are using negative value for cri since maximum is white and minimum is black when cmap is set to hot
+	
+
+	ax2=fig.add_subplot(1,2,2) #we create a 1 x 2 subplot and assigning ax2 to the second subplot
+	ax2.set_title("Pedestrian locus animation") #sets title to the subplot
+	ax2.set_xlim(0,x_dim)
+	ax2.set_ylim(0,y_dim)
+	for row in walls.itertuples(): #for loop to iterate through the dataframe containing the wall coordinates
+		ax2.plot([row[1],row[3]],[row[2],row[4]]) #this will plot a line using the x and y coordinates of the wall entered in the excel
+	scat=ax2.scatter([0],[0],s=50,lw=0,facecolor='0.5')	
+
+	anim=animation.FuncAnimation(fig,update_heat_map,fargs=(ax1,im),frames=400,interval=500)
+	anim2=animation.FuncAnimation(fig,update_scatter_plot,fargs=(ax2,scat),frames=400,interval=500)
+
 	fig.show() #displays the plot
 
-	for i in xrange (loops): #iterates through the loops
-		sfm(ped,i) #updates the pedestrian positions and velocity by calculating the forces 
-		#heat_maps(ped) #generate heat map with the current pedestrian state
-		#animate(ped)
-		add_dataframe(positionX,positionY,impatience)
-		
-	#save_as_excel(positionX,positionY,impatience)
