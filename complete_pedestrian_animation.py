0be741940cdf56d176ed6bdef9e763ee2cc1ebdd
Ravikiran <raviramesh.kiran97@gmail.com>
2017-07-12 18:11:13 +0530

    commented everything

--- complete_pedestrian_animation.py
+++ complete_pedestrian_animation.py
@@ -8,19 +8,19 @@ from complete_pedestrian_simulation import loops,t_int,walls,n_ped,v_desired_max
 
 ### GLOBAL VARIABLES ###
 
-x_dim=max(walls["X1"].max(),walls["X2"].max())  
-y_dim=max(walls["Y1"].max(),walls["Y2"].max())
+x_dim=max(walls["X1"].max(),walls["X2"].max())  #x_dim is the maximum value of x dimension of the walls
+y_dim=max(walls["Y1"].max(),walls["Y2"].max())  #y_dim is the maximum value of y dimension of the walls
 
 grid_size=1  #size of grid in the heat map (in meters)
 mf=1/float(grid_size) #multiplication factor since the number of grids in the heat map increases with decrease in grip size
 
 """The weightage of various factors on the Crowd risk Index (CRI)"""
-imp_weight=2 #impatience factor
-density_weight=1 #pedestrian density factor
-b_weight=0
-p_weight=0
+imp_weight=2 #impatience
+density_weight=1 #pedestrian density
+b_weight=0 #scalar sum of border force
+p_weight=0 #scalar sum of pedestrian repulsion force
 
-"""The threshold values of various factors influencing CRI, beyond which, it is dangerous"""
+"""The threshold/maximum values of various factors influencing CRI, beyond which, it is dangerous"""
 imp_threshold=1     
 density_threshold=3
 b_threshold=0
@@ -30,52 +30,53 @@ max_cri=imp_threshold*imp_weight+density_threshold*density_weight \
         + b_threshold*b_weight + p_threshold*p_weight        #The maximum value the CRI can have without danger
 
 
-x=pd.ExcelFile("Pedestrian_details.xlsx").parse("X positions").as_matrix()
-y=pd.ExcelFile("Pedestrian_details.xlsx").parse("Y positions").as_matrix()
-vx=pd.ExcelFile("Pedestrian_details.xlsx").parse("X velocity").as_matrix()
-vy=pd.ExcelFile("Pedestrian_details.xlsx").parse("Y velocity").as_matrix()
-ax=pd.ExcelFile("Pedestrian_details.xlsx").parse("X acceleration").as_matrix()
-ay=pd.ExcelFile("Pedestrian_details.xlsx").parse("Y acceleration").as_matrix()
-b_net=pd.ExcelFile("Pedestrian_details.xlsx").parse("border force").as_matrix()
-p_net=pd.ExcelFile("Pedestrian_details.xlsx").parse("pedestrian force").as_matrix()
+x=pd.ExcelFile("Pedestrian_details.xlsx").parse("X positions").as_matrix() #read x values from the excel file as a numpy array
+y=pd.ExcelFile("Pedestrian_details.xlsx").parse("Y positions").as_matrix() #read y values from the excel file as a numpy array
+vx=pd.ExcelFile("Pedestrian_details.xlsx").parse("X velocity").as_matrix() #read velocity_x values from the excel file as a numpy array
+vy=pd.ExcelFile("Pedestrian_details.xlsx").parse("Y velocity").as_matrix() #read velocity_y values from the excel file as a numpy array
+ax=pd.ExcelFile("Pedestrian_details.xlsx").parse("X acceleration").as_matrix() #read acceleration_x values from the excel file as a numpy array
+ay=pd.ExcelFile("Pedestrian_details.xlsx").parse("Y acceleration").as_matrix() #read acceleration_y values from the excel file as a numpy array
+b_net=pd.ExcelFile("Pedestrian_details.xlsx").parse("border force").as_matrix() #read net border force values from the excel file as a numpy array
+p_net=pd.ExcelFile("Pedestrian_details.xlsx").parse("pedestrian force").as_matrix() #read net pedestrian force values from the excel file as a numpy array
 
 ### FUNCTIONS USED ###
 
 
 def update_scatter(i,ax1,scat):
-	ax1.set_title("Pedestrian path : "+str(i))
-	ped_xy=[]
+	'''
+	This method is used to update the scatter plot after every frame
+	''' 
+	ped_xy=[] #list containing the coordinates (x,y) of every pedestrian
 	for j in xrange(n_ped):
-		if vx[i][j]!=0 and vy[i][j]!=0:
-			ped_xy.append([x[i][j],y[i][j]])
+		if vx[i][j]!=0 and vy[i][j]!=0: #if the velocity is zero, the pedestrian has not yet emerged
+			ped_xy.append([x[i][j],y[i][j]]) 
 
-	scat.set_offsets(ped_xy)
+	scat.set_offsets(ped_xy) #this method sets the data for the scatter plot
 	return scat,
 
 
 def update_heat(i,ax2,im):
-	ax2.set_title("Heat map : "+str(i))
 	
 	cri=np.zeros(shape=(int(y_dim*mf),int(x_dim*mf))) #creates a numpy array for each cell of the heat map, and element value 0
-	density=np.zeros(shape=(int(y_dim*mf),int(x_dim*mf)))
-	impatience=np.zeros(shape=(int(y_dim*mf),int(x_dim*mf)))
-	border_forces=np.zeros(shape=(int(y_dim*mf),int(x_dim*mf)))
-	ped_forces=np.zeros(shape=(int(y_dim*mf),int(x_dim*mf)))
+	density=np.zeros(shape=(int(y_dim*mf),int(x_dim*mf))) #initial value of density
+	impatience=np.zeros(shape=(int(y_dim*mf),int(x_dim*mf))) #initial value of impatience
+	border_forces=np.zeros(shape=(int(y_dim*mf),int(x_dim*mf))) #initial value of border force
+	ped_forces=np.zeros(shape=(int(y_dim*mf),int(x_dim*mf))) #initial value of pedestrian repulsion force
 	
 	for j in xrange(n_ped):
-		if vx[i][j]!=0 and vy[i][j]!=0:
-			density[int(math.floor(y[i][j]))][int(math.floor(x[i][j]))]-=1
+		if vx[i][j]!=0 and vy[i][j]!=0:  #if the velocity is zero, the pedestrian has not yet emerged
+			density[int(math.floor(y[i][j]))][int(math.floor(x[i][j]))]+=1 #density value increments in the cells where the pedestrian is present 
 			
 			imp=1-(math.sqrt(vx[i][j]**2 + vy[i][j]**2))/v_desired_max
-			impatience[int(math.floor(y[i][j]*mf))][int(math.floor(x[i][j]*mf))]-=imp
+			impatience[int(math.floor(y[i][j]*mf))][int(math.floor(x[i][j]*mf))]+=imp #adds the value of impatience to the cell where pedestrian is present
 
-			border_forces[int(math.floor(y[i][j]))][int(math.floor(x[i][j]))]-=b_net[i][j]
-			ped_forces[int(math.floor(y[i][j]))][int(math.floor(x[i][j]))]-=p_net[i][j]
+			border_forces[int(math.floor(y[i][j]))][int(math.floor(x[i][j]))]+=b_net[i][j] #adds the value of border force to the cell where pedestrian is present
+			ped_forces[int(math.floor(y[i][j]))][int(math.floor(x[i][j]))]+=p_net[i][j] #adds the value of pedestrian force to the cell where pedestrian is present
 
 
-	cri=density_weight*density + imp_weight*impatience + b_weight*border_forces + p_weight * ped_forces
+	cri=density_weight*density + imp_weight*impatience + b_weight*border_forces + p_weight * ped_forces #calculation of CRI
 
-	im.set_data(cri)
+	im.set_data(-cri) #sets CRI to the heat map
 	return im,
 
 
@@ -89,7 +90,7 @@ if __name__ == '__main__':
 	plt.suptitle("Animation graphs") #set title for graph
 
 	ax1=fig.add_subplot(1,2,1) #create 1 x 2 subplot in the graph and assigns the first subplot to ax1
-	ax1.set_title("Pedestrian path")
+	ax1.set_title("Pedestrian path") #set title for the subplot
 	ax1.set_xlim(0,x_dim)  #set x limits to first subplot
 	ax1.set_ylim(0,y_dim)  #set y limits to first subplot
 	for row in walls.itertuples(): #for loop to iterate through the dataframe containing the wall coordinates
@@ -100,18 +101,26 @@ if __name__ == '__main__':
 	### SETUP HEAT MAP ###
 
 	ax2=fig.add_subplot(1,2,2) #create 1 x 2 subplot in the graph and assigns the second subplot to ax2
-	ax2.set_title("Heat map")
+	ax2.set_title("Heat map") #set title for the subplot
 	ax2.set_xlim(0,x_dim*mf)  #set x limits to second subplot
 	ax2.set_ylim(0,y_dim*mf)  #set y limits to second subplot
 	ax2.get_xaxis().set_visible(False) #removes the scale display of the heat map
 	ax2.get_yaxis().set_visible(False) #removes the scale display of the heat map
 	for row in walls.itertuples(): #for loop to iterate through the dataframe containing the wall coordinates
 		ax2.plot([row[1],row[3]],[row[2],row[4]]) #this will plot a line using the x and y coordinates of the wall entered in the excel
-	cri=np.zeros(shape=(int(y_dim*mf),int(x_dim*mf)))
-	im=ax2.imshow(cri,cmap='hot',interpolation='nearest',vmin=-(max_cri),vmax=0)
+	cri=np.zeros(shape=(int(y_dim*mf),int(x_dim*mf))) #initial value of CRI
+	im=ax2.imshow(cri,cmap='hot',interpolation='nearest',vmin=-(max_cri),vmax=0) #imageplot used to plot the heatmap
+	#we use negative of CRI since when cmap is set to hot, the heatmap takes white for maximum and black for minimum
 
 
 	### RUN ANIMATION ###
-	anim=animation.FuncAnimation(fig,update_scatter,fargs=(ax1,scat),frames=loops,interval=int(t_int*1000))
-	anim2=animation.FuncAnimation(fig,update_heat,fargs=(ax2,im),frames=loops,interval=int(t_int*1000))
+	'''
+	Arguements for the below functions:
+	fig - figure of theplot we want to animate
+	update function -  function that updates the plot after every frame
+	frame- number of frames for which you need to animation to run
+	interval - interval time between two frames
+	'''
+	anim=animation.FuncAnimation(fig,update_scatter,fargs=(ax1,scat),frames=loops,interval=int(t_int*1000)) #animates the scatter plot
+	anim2=animation.FuncAnimation(fig,update_heat,fargs=(ax2,im),frames=loops,interval=int(t_int*1000)) #animates the heat map
 	plt.show()
--- complete_pedestrian_simulation.py
+++ complete_pedestrian_simulation.py
@@ -13,7 +13,7 @@ import random
 """ GLOBAL VARIABLES"""
 
 t_int=0.1     #interval time (in seconds)
-t_total=20     #total running time (in seconds)
+t_total=180     #total running time (in seconds)
 loops=int(t_total/t_int) #calculates the number of loops to be performed
 
 n_ped=200   #number of pedestrians
@@ -58,110 +58,156 @@ class Pedestrian:
 	def set_path(self):
 		self.room=random.randint(1,rooms) #randomly chooses a room in which the pedestrian starts
 		if self.room==1:
-			self.x=random.uniform(2,12) 
+			self.x=random.uniform(2,12)  #places the pedestrian randomply within room 1
 			self.y=random.uniform(15,25)
-			self.path=points.parse("Path1").as_matrix()  #chooses the first path for 
+			self.path=points.parse("Path1").as_matrix()  #pedestrians from room 1 follow path 1
 			self.checkpoints=self.path.shape[0]   #number of points the pedestrian have to cross
 
 		elif self.room==2:
-			self.x=random.uniform(25,35)
+			self.x=random.uniform(25,35)  #places the pedestrian randomply within room 2
 			self.y=random.uniform(30,40)
-			self.path=points.parse("Path2").as_matrix()
+			self.path=points.parse("Path2").as_matrix() #pedestrians from room 2 follow path 2
 			self.checkpoints=self.path.shape[0]   #number of points the pedestrian have to cross
 
 		elif self.room==3:
-			self.x=random.uniform(25,35)
+			self.x=random.uniform(25,35)  #places the pedestrian randomply within room 3
 			self.y=random.uniform(15,25)
-			self.path=points.parse("Path3").as_matrix()
+			self.path=points.parse("Path3").as_matrix() #pedestrians from room 3 follow path 3
 			self.checkpoints=self.path.shape[0]   #number of points the pedestrian have to cross
 
 		else:
-			self.x=random.uniform(40,50)
+			self.x=random.uniform(40,50)  #places the pedestrian randomply within room 4
 			self.y=random.uniform(5,15)
-			self.path=points.parse("Path4").as_matrix()
+			self.path=points.parse("Path4").as_matrix() #pedestrians from room 4 follow path 4
 			self.checkpoints=self.path.shape[0]   #number of points the pedestrian have to cross
 
 
 
 	def calc_desired_velocity(self):
 
-		self.imp=1-(math.sqrt(self.vx**2+self.vy**2))/self.vd_net
-		self.vd_net= (1-self.imp)*self.vd_net + self.imp*v_desired_max
+		self.imp=1-(math.sqrt(self.vx**2+self.vy**2))/self.vd_net  #calculates impatience
+		self.vd_net= (1-self.imp)*self.vd_net + self.imp*v_desired_max  #calculates desired velocity at every instant (since it changes with impatience)
 		
-		newside=self.calc_side()
-		if self.side*newside<0:
-			if self.point<self.checkpoints-1:
-				self.point+=1
-				self.side=self.calc_side()
+		newside=self.calc_side() #calculates the side the pedestrian is on, with respect to the checkpoint
+		if self.side*newside<0: #checks if there is any change in sign of the side. A change in sign indicates that the pedestrian has crossed the checkpoint
+			if self.point<self.checkpoints-1: #pedestrian is yet to go through all the checkpoints
+				self.point+=1 #increments the number of points the pedestrian has crossed
+				self.side=self.calc_side() #updates the side he's in, with respect to the new checkpoint
 			else:
-				self.end=True
-		x1=self.path[self.point][0]
+				self.end=True #if he has crossed all the checkpoints
+
+		x1=self.path[self.point][0] #read the coordinates of the checkpoints
 		y1=self.path[self.point][1]
 		x2=self.path[self.point][2]
 		y2=self.path[self.point][3]
 
-		A=y1-y2
+		A=y1-y2  #converting the line equation into Ax + By + C = 0 form
 		B=x2-x1
 		C=y2*x1-x2*y1	
 
 		m=self.x
 		n=self.y
 		
-		dist=abs((A*m+B*n+C)/math.sqrt(A*A+B*B))
+		dist=abs((A*m+B*n+C)/math.sqrt(A*A+B*B)) #perpendicular distance between the checkpoint and the pedestrian
 
 		t_x= m - A*(A*m+B*n+C)/(A*A+B*B)  #coordinates of the foot of the perpendicular from the pedestrian location (m,n)	
 		t_y= n - B*(A*m+B*n+C)/(A*A+B*B)  
 
-		if (t_x-x1)*(t_x-x2)>=0 and (t_y-y1)*(t_y-y2)>=0:
-			t_x=(x1+x2)/2
+		if (t_x-x1)*(t_x-x2)>=0 and (t_y-y1)*(t_y-y2)>=0: #if foot of the perpendicular doesnt lie on the wall, the pedestrian goes to the middle point of the checkpoint
+                                                          #this is especially useful incase of evacuation		
+			t_x=(x1+x2)/2 
 			t_y=(y1+y2)/2
 
 		u_x=(t_x-m)/dist   #x component of unit vector from the pedestrian to the desired line
 		u_y=(t_y-n)/dist   #y component of unit vector from the pedestrian to the desired line
 
-		self.vdx=self.vd_net*u_x
+		self.vdx=self.vd_net*u_x  #assig desired velocity in x and y direction
 		self.vdy=self.vd_net*u_y		
 
 	def init_constants(self):
+		"""
+		This method initialises the constants particular to each pedestrian
+		"""
 		
-		self.starting_frame=random.randint(0,loops/2)
+		self.starting_frame=random.randint(0,loops/2)  #the frame at which the pedestrian will emerge
 
-		self.category=random.randint(1,4)
+		self.category=random.randint(1,4) #randomly assigns a category for the pedestrian, and that category influences other constants of the pedestrian
 		if self.category==1:
-			self.vd_net=random.uniform(1,1.1)
+			self.vd_net=random.uniform(1,1.1) #net desired velocity
+			self.t_relax=random.uniform(0.9,1.1) #relacation time
+			self.rad=random.uniform(0.5,0.6) #radius of the pedestrian
+
+			self.a_b=random.uniform(0.9,1.1) #constants used to calculate border repulsion
+			self.b_b=random.uniform(0.27,0.33)
+
+			self.a1=random.uniform(0.027,0.033) #constants used to calculate pedestrian repulsion
+			self.b1=random.uniform(0.18,0.22)
+			self.a2=random.uniform(0.045,0.055)
+			self.b2=random.uniform(0.18,0.22)
+
 		elif self.category==2:
 			self.vd_net=random.uniform(1.1,1.2)
+			self.t_relax=random.uniform(0.9,1.1) #relacation time
+			self.rad=random.uniform(0.5,0.6) #radius of the pedestrian
+
+			self.a_b=random.uniform(0.9,1.1) #constants used to calculate border repulsion
+			self.b_b=random.uniform(0.27,0.33)
+
+			self.a1=random.uniform(0.027,0.033) #constants used to calculate pedestrian repulsion
+			self.b1=random.uniform(0.18,0.22)
+			self.a2=random.uniform(0.045,0.055)
+			self.b2=random.uniform(0.18,0.22)
+
 		elif self.category==3:
 			self.vd_net=random.uniform(1.2,1.3)
+			self.t_relax=random.uniform(0.9,1.1) #relacation time
+			self.rad=random.uniform(0.5,0.6) #radius of the pedestrian
+
+			self.a_b=random.uniform(0.9,1.1) #constants used to calculate border repulsion
+			self.b_b=random.uniform(0.27,0.33)
+
+			self.a1=random.uniform(0.027,0.033) #constants used to calculate pedestrian repulsion
+			self.b1=random.uniform(0.18,0.22)
+			self.a2=random.uniform(0.045,0.055)
+			self.b2=random.uniform(0.18,0.22)
+
 		elif self.category==4:
 			self.vd_net=random.uniform(1.3,1.4)
+			self.t_relax=random.uniform(0.9,1.1) #relacation time
+			self.rad=random.uniform(0.5,0.6) #radius of the pedestrian
 
-		self.t_relax=random.uniform(0.9,1.1)
-		self.rad=random.uniform(0.5,0.6)
+			self.a_b=random.uniform(0.9,1.1) #constants used to calculate border repulsion
+			self.b_b=random.uniform(0.27,0.33)
 
-		self.a_b=random.uniform(0.9,1.1)
-		self.b_b=random.uniform(0.27,0.33)
+			self.a1=random.uniform(0.027,0.033) #constants used to calculate pedestrian repulsion
+			self.b1=random.uniform(0.18,0.22)
+			self.a2=random.uniform(0.045,0.055)
+			self.b2=random.uniform(0.18,0.22)
 
-		self.a1=random.uniform(0.027,0.033)
-		self.b1=random.uniform(0.18,0.22)
-		self.a2=random.uniform(0.045,0.055)
-		self.b2=random.uniform(0.18,0.22)
 
+		
 	def set_initial_conditions(self):
-		self.point=0
-		self.end=False
-		self.start=False
+		"""
+		THis method sets up the initial conditions which are common to all the pedestrians
+		"""
+		self.point=0 #the number of checkpoints the pedestrian crossed
+		self.end=False #tells whether the pedestrian completed their path
+		self.start=False #tells whether the pedestrian started / emerged
 
-		self.b_net=0
-		self.p_net=0
+		self.b_net=0 #scalar sum of border forces acting on the pedestrian
+		self.p_net=0 #scalar sum of the pedestrian forces acting on the pedestrian
 
 
 	def calc_side(self):
+		"""
+		This method calculates the side at which the pedestrian is present with respect to his/her next checkpoint
+		For any line, all the points on one side of the line has positive value and the ones on the other side has a negative value
+		"""
 		x1=self.path[self.point][0]
 		x2=self.path[self.point][2]
 		y1=self.path[self.point][1]
 		y2=self.path[self.point][3]
-		return (y1-y2)*(self.x-x1)+(self.y-y1)*(x2-x1)
+		return (y1-y2)*(self.x-x1)+(self.y-y1)*(x2-x1) #returns the value of the equation of the line at (x,y)
 
 
 
@@ -219,12 +265,12 @@ def border_repulsion(p):
 
 		if dist<border_threshold: #walls outside this threshold wont influence
 			if (t_x-walls_m[i][0])*(t_x-walls_m[i][2]) <= 0 and (t_y-walls_m[i][1])*(t_y-walls_m[i][3]) <= 0: #the foot must lie on the wall
-				f_x+=p.a_b*math.exp((p.rad-dist)/p.b_b)*u_x
+				f_x+=p.a_b*math.exp((p.rad-dist)/p.b_b)*u_x #calculate and add forces vectorially
 				f_y+=p.a_b*math.exp((p.rad-dist)/p.b_b)*u_y
 
-				p.b_net+=math.sqrt(f_x**2 + f_y**2)
+				p.b_net+=math.sqrt(f_x**2 + f_y**2) #scalar addition of all the forces acting on the pedestrian
 
-	return f_x,f_y				
+	return f_x,f_y	#return the forces in x and y direction	
 
 def pedestrian_repulsion(ped,p):
 	"""
@@ -248,7 +294,7 @@ def pedestrian_repulsion(ped,p):
 
 
 	for i in xrange(n_ped): #iterates through all the pedestrians from the list
-		if ped[i].start==True and ped[i].end==False: 
+		if ped[i].start==True and ped[i].end==False:  #considers only the pedestrians that ae active now
 			
 			dist=math.sqrt((ped[i].x-p.x)**2+(ped[i].y-p.y)**2) #distance of the i th pedestrian from pedestrian p 
 		
@@ -263,7 +309,7 @@ def pedestrian_repulsion(ped,p):
 				#y component of force on p due to the i th pedestrian 
 				f_y+=p.a1*math.exp((r-dist)/p.b1)*ny*(lam+(1-lam)*(1+cos)/2)+p.a2*math.exp((r-dist)/p.b2)*ny
 
-				p.p_net+=math.sqrt(f_x**2 + f_y**2)
+				p.p_net+=math.sqrt(f_x**2 + f_y**2) #scalar addition of all the forces acting on the pedestrian
 
 		
 	return f_x,f_y
@@ -280,10 +326,10 @@ def sfm(ped,frame):
 		#It then updates the positions and velocities of the pedestrians using kinematic equations
 		
 
-		if ped[i].start==True and ped[i].end==False:
-			ped[i].calc_desired_velocity()
-			f_total=[sum(x) for x in zip( driving_force(ped[i]) , border_repulsion(ped[i]) , pedestrian_repulsion(ped,ped[i]))]
-			ped[i].ax=f_total[0]/m_ped
+		if ped[i].start==True and ped[i].end==False: #only considers the pedestrians that are active
+			ped[i].calc_desired_velocity() #calculates the desired velocity
+			f_total=[sum(x) for x in zip( driving_force(ped[i]) , border_repulsion(ped[i]) , pedestrian_repulsion(ped,ped[i]))] #adds the 3 forces
+			ped[i].ax=f_total[0]/m_ped #acceleration in the x and y directions
 			ped[i].ay=f_total[1]/m_ped
 
 			ped[i].x+=ped[i].vx*t_int+0.5*ped[i].ax*t_int*t_int  # s = ut + 0.5 at^2 in the x direction
@@ -291,8 +337,8 @@ def sfm(ped,frame):
 			ped[i].vx+=ped[i].ax*t_int  # v = u + at in the x direction
 			ped[i].vy+=ped[i].ay*t_int  # v = u + at in the y direction
 		else:
-			if frame==ped[i].starting_frame:
-				ped[i].start=True
+			if frame==ped[i].starting_frame: #the pedestrian emerges after a particular frame
+				ped[i].start=True #pedestrian has started
 
 		
 
@@ -309,12 +355,15 @@ def generate_pedestrians(ped):
 		ped.append(Pedestrian())
 
 def save_property_matrix(ped):
+	"""
+	Create a property matrix, that contains the properties unique to every pedestrian
+	"""
 
-	columns=[]
+	columns=[] #string list containing the column names in the dataframe
 	for i in xrange(n_ped):
 		columns.append("pedestrian "+str(i+1))
 
-	indices=[]
+	indices=[] #string list containing the index names in the dataframe
 	indices.append("Desired velocity")
 	indices.append("Starting frame")
 	indices.append("Category")
@@ -328,7 +377,7 @@ def save_property_matrix(ped):
 	indices.append("b1")
 	indices.append("b2")
 
-	property_list=[]
+	property_list=[] #a lists of lists containing all the pedestrian properties
 	property_list.append([a.vd_net for a in ped])
 	property_list.append([a.starting_frame for a in ped])
 	property_list.append([a.category for a in ped])
@@ -342,22 +391,25 @@ def save_property_matrix(ped):
 	property_list.append([a.b1 for a in ped])
 	property_list.append([a.b2 for a in ped])
 
-	property_matrix=pd.DataFrame(property_list,columns=columns,index=indices)
-	property_matrix.to_excel("Pedestrian_properties.xlsx",sheet_name="Sheet1")
+	property_matrix=pd.DataFrame(property_list,columns=columns,index=indices) #converts the list into a dataframe
+	property_matrix.to_excel("Pedestrian_properties.xlsx",sheet_name="Sheet1") #saves the dataframe as excel
 
-def add_dataframe(ped):
+def add_list(ped):
+	"""
+	This method is for adding pedestrian status after each interval of time
+	"""
 
-	positionX.append([a.x for a in ped]) #adds the x position of all the pedestrians to the data frame
-	positionY.append([a.y for a in ped]) #adds the y position of all the pedestrians to the data frame
-	velocityX.append([a.vx for a in ped]) #adds the x velocity of all the pedestrians to the data frame
-	velocityY.append([a.vy for a in ped]) #adds the y velocity of all the pedestrians to the data frame
-	accelerationX.append([a.ax for a in ped]) #adds the x acceleration of all the pedestrians to the data frame
-	accelerationY.append([a.ay for a in ped]) #adds the y acceleration of all the pedestrians to the data frame
-	border_net.append([a.b_net for a in ped])
-	ped_net.append([a.p_net for a in ped])
+	positionX.append([a.x for a in ped]) #adds the x position of all the pedestrians to the list
+	positionY.append([a.y for a in ped]) #adds the y position of all the pedestrians to the list
+	velocityX.append([a.vx for a in ped]) #adds the x velocity of all the pedestrians to the list
+	velocityY.append([a.vy for a in ped]) #adds the y velocity of all the pedestrians to the list
+	accelerationX.append([a.ax for a in ped]) #adds the x acceleration of all the pedestrians to the list
+	accelerationY.append([a.ay for a in ped]) #adds the y acceleration of all the pedestrians to the list
+	border_net.append([a.b_net for a in ped]) #adds the border forces of all the pedestrians to the list
+	ped_net.append([a.p_net for a in ped]) #adds the pedestrian forces of all the pedestrians to the list
 	
 	for a in ped:
-		a.p_net=a.b_net=0
+		a.p_net=a.b_net=0 #resets the value
 	
 
 
@@ -365,18 +417,18 @@ def save_as_excel():
 	"""
 	This method saves the pandas dataframe into excel.
 	"""
-	columns=[]
+	columns=[] #string list containing the column names in the dataframe
 	for i in xrange(n_ped):
-		columns.append("pedestrian "+str(i+1))
+		columns.append("pedestrian "+str(i+1)) 
 
-	px=pd.DataFrame(np.array(positionX),columns=columns)
-	py=pd.DataFrame(np.array(positionY),columns=columns)
-	vx=pd.DataFrame(np.array(velocityX),columns=columns)
-	vy=pd.DataFrame(np.array(velocityY),columns=columns)
-	ax=pd.DataFrame(np.array(accelerationX),columns=columns)
-	ay=pd.DataFrame(np.array(accelerationY),columns=columns)
-	b_net=pd.DataFrame(np.array(border_net),columns=columns)
-	p_net=pd.DataFrame(np.array(ped_net),columns=columns)
+	px=pd.DataFrame(np.array(positionX),columns=columns) #creates a dataftame from the given list
+	py=pd.DataFrame(np.array(positionY),columns=columns) #creates a dataftame from the given list
+	vx=pd.DataFrame(np.array(velocityX),columns=columns) #creates a dataftame from the given list
+	vy=pd.DataFrame(np.array(velocityY),columns=columns) #creates a dataftame from the given list
+	ax=pd.DataFrame(np.array(accelerationX),columns=columns) #creates a dataftame from the given list
+	ay=pd.DataFrame(np.array(accelerationY),columns=columns) #creates a dataftame from the given list
+	b_net=pd.DataFrame(np.array(border_net),columns=columns) #creates a dataftame from the given list
+	p_net=pd.DataFrame(np.array(ped_net),columns=columns) #creates a dataftame from the given list
 
 	writer=pd.ExcelWriter("Pedestrian_details.xlsx") #creates an excel writes
 
@@ -386,8 +438,8 @@ def save_as_excel():
 	vy.to_excel(writer,sheet_name="Y velocity") #writes the dataframe into the excel file in the given sheet
 	ax.to_excel(writer,sheet_name="X acceleration") #writes the dataframe into the excel file in the given sheet
 	ay.to_excel(writer,sheet_name="Y acceleration") #writes the dataframe into the excel file in the given sheet
-	b_net.to_excel(writer,sheet_name="border force")
-	p_net.to_excel(writer,sheet_name="pedestrian force")
+	b_net.to_excel(writer,sheet_name="border force") #writes the dataframe into the excel file in the given sheet
+	p_net.to_excel(writer,sheet_name="pedestrian force") #writes the dataframe into the excel file in the given sheet
 
 	writer.save() #saves the excel file in the same diretory as this script
 
@@ -401,20 +453,20 @@ if __name__=="__main__":
 	
 	ped=[]  #list of pedestrian instances
 
-	generate_pedestrians(ped)  #generate some pedestrians initially
-	save_property_matrix(ped)
+	generate_pedestrians(ped)  #generate pedestrians
+	save_property_matrix(ped)  #save property matrix
 
-	positionX=[]
-	positionY=[]
-	velocityX=[]
-	velocityY=[]
-	accelerationX=[]
-	accelerationY=[]
-	border_net=[]
-	ped_net=[]
+	positionX=[]  #list containing pedestrian x positions
+	positionY=[]  #list containing pedestrian y positions
+	velocityX=[]  #list containing pedestrian x velocities
+	velocityY=[]  #list containing pedestrian y velocities
+	accelerationX=[]  #list containing pedestrian x acceleration
+	accelerationY=[]  #list containing pedestrian y acceleration
+	border_net=[]  #list containing border repulsion force experienced by each pedestrian
+	ped_net=[]  #list containing pedestrian repulsion force experienced by each pedestrian
 	
 	for frame in xrange (loops): #iterates through the loops
 		sfm(ped,frame) #updates the pedestrian positions and velocity by calculating the forces 
-		add_dataframe(ped)
+		add_list(ped) #adds pedestrian data to the list
 	
-	save_as_excel()
+	save_as_excel() #saves all details in excel for animation
